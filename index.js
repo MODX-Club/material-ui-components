973750c832a1ac225e4d2b76f5da8fe230d1528f
Nikolay Lanets <fi1osof@fi1osofski.modxclub.local>
2017-05-15 21:49:42 +0300

    1.4.0

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-export {default as Avatar} from './src/Avatar/';
-export {default as Auth} from './src/Auth/';
-export {default as GooglePlaceAutocomplete} from './src/GooglePlaceAutocomplete/';
-export {default as Informer} from './src/Informer/';
-export {default as Popover} from './src/Popover/';
-export {default as RichTextEditor} from './src/RichTextEditor/';
-export {default as SelectField} from './src/SelectField/';
-export {default as Snackbar} from './src/Snackbar/';
-export {default as TextArea} from './src/TextArea/';
+// export {default as Avatar} from './src/Avatar/';
+// export {default as Auth} from './src/Auth/';
+// export {default as GooglePlaceAutocomplete} from './src/GooglePlaceAutocomplete/';
+// export {default as Informer} from './src/Informer/';
+// export {default as Popover} from './src/Popover/';
+// export {default as RichTextEditor} from './src/RichTextEditor/';
+// export {default as SelectField} from './src/SelectField/';
+// export {default as Snackbar} from './src/Snackbar/';
+// export {default as TextArea} from './src/TextArea/';
